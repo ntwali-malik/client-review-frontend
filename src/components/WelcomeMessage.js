c437b622897b55a6f62aae9c31ff989764e1ac89
Maliki <aboulmalikntwari@gmail.com>
2025-03-17 09:13:30 +0200

    new changes

--- src/components/WelcomeMessage.js
+++ src/components/WelcomeMessage.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './WelcomeMessage.css';
-import logo from '../public/logoF.png'; // Adjust the path if necessary
 
 function WelcomeMessage() {
   return (
     <div className="welcome-message">
-      <img src={logo} alt="Fabritech Logo" className="company-logo" />
+      <img src={`${process.env.PUBLIC_URL}/logoF.png`} alt="Fabritech Logo" className="company-logo" />
       <h1>Welcome to Fabritech's Feedback System!</h1>
       <p>Your thoughts and opinions matter to us. Please take a moment to share your feedback below.</p>
       <p>Thank you for helping us improve our services!</p>
